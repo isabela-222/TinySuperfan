b6f29220fbf7e0042fe1ab063c9be8628e44028d
isabela-222 <marquez3549@gmail.com>
2024-11-14 13:21:46 -0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,52 @@
+function BaOne() {
+  document.getElementById("video").src =
+    "https://www.youtube.com/embed/7w7BXhCAbhU?autoplay=1";
+}
+function BaTwo() {
+  document.getElementById("video").src =
+    "https://www.youtube.com/embed/1Np5QTChT1o?autoplay=1";
+}
+function BaThree() {
+  document.getElementById("video").src =
+    "https://www.youtube.com/embed/QfoDVGIKMLw?autoplay=1";
+}
+function BaFour() {
+  document.getElementById("video").src =
+    "https://www.youtube.com/embed/pCummqoriFU?autoplay=1";
+}
+function BaFive() {
+  document.getElementById("video").src =
+    "https://www.youtube.com/embed/C89SFsgLDd8?autoplay=1";
+}
+function BaSix() {
+  document.getElementById("video").src =
+    "https://www.youtube.com/embed/cH8LvsZBw0I?autoplay=1";
+}
+function BaSeven() {
+  document.getElementById("video").src =
+    "https://www.youtube.com/embed/4PFS6fQ01QE?autoplay=1";
+}
+function BaEight() {
+  document.getElementById("video").src =
+    "https://www.youtube.com/embed/10Xay2halGU?autoplay=1";
+}
+function BaNine() {
+  document.getElementById("video").src =
+    "https://www.youtube.com/embed/5tYP4IC6g-k?autoplay=1";
+}
+function BaTen() {
+  document.getElementById("video").src =
+    "https://www.youtube.com/embed/ss-kAc0SLvE?autoplay=1";
+}
+function BaEleven() {
+  document.getElementById("video").src =
+    "https://www.youtube.com/embed/_SKXkIC7dMM?autoplay=1";
+}
+function BaTwelve() {
+  document.getElementById("video").src =
+    "https://www.youtube.com/embed/-1L5dZ2WrMM?autoplay=1";
+}
+
+function PlayMusic() {
+  document.getElementById("song").play();
+}
